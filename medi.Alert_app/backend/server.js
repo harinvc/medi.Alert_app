eb1a58137dc5a1e56a319216ae5e3b4bab25d5c8
sathana-v <sathanav.19cse@kongu.edu>
2026-08-09 08:37:03 +0530

    Fix socket CORS

--- medi.Alert_app/backend/server.js
+++ medi.Alert_app/backend/server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 // Initialize Socket.io Server
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+    origin: '*',
     methods: ['GET', 'POST']
   }
 });
